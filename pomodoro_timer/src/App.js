020aa45e7dd1e80115c8dbee1e831d145570d5d0
eduardoaguilar96 <eduaguilar96@gmail.com>
2022-10-26 21:46:21 +0200

    maquetacion

--- pomodoro_timer/src/App.js
+++ pomodoro_timer/src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
+<link href="heroes.css" rel="stylesheet"></link>
+
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="titulo">POMODORO TIMER</h1>
+        <h1 class="titulo">POMODORO TIMER</h1>
         <div className="Card">
-          <h1>HOLA</h1>
-          <h2>GGGGGGGGGGGG</h2>
-          <h2>GGGGGGGGGGGG</h2>
-          <h2>GGGGGGGGGGGG</h2>
-          <h2>GGGGGGGGGGGG</h2>
-          <button class="btn btn-primary" type="button">Button</button>
+          <h2>25:00</h2>
           
-          
-          
+
+
+          <button className="btn-primary" type="button">START</button>
+          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">RESET</button>
+
+
+
         </div>
-        
+
       </header>
     </div>
   );
